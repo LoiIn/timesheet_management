d7608d5d3b656e564ad9548615708ba3b9789894
loi-can <loi-can.dimage.co.jp>
2021-05-05 18:38:04 +0900

    add basic middleware

--- routes/web.php
+++ routes/web.php
@@ -11,40 +11,42 @@
 |
 */
 
-//home page
-Route::get('/', 'PageController@home')->name('home.index');
-Route::get('home', 'PageController@home')->name('home.index');
-
-//sign in - sign out - sign-up
-Route::prefix('')->group(function () {
+Route::prefix('/')->middleware('logout')->group(function () {
     Route::get('sign-in', 'Auth\LoginController@login')->name('login.index');
     Route::post('sign-in', 'Auth\LoginController@postLogin')->name('login');
-    Route::get('sign-out', 'Auth\LoginController@logout')->name('logout');
     Route::get('sign-up', 'Auth\RegisterController@register')->name('register.index');
-    Route::post('sign-up', 'Auth\RegisterController@postRegister')->name('register');   
+    Route::post('sign-up', 'Auth\RegisterController@postRegister')->name('register');
 });
 
-//user
-Route::prefix('user-profiles')->group(function () {
-    Route::get('/', 'UserController@index')->name('user.index');
-    Route::get('/edit', 'UserController@edit')->name('user.edit');
-    Route::post('/edit', 'UserController@update')->name('user.update');
-});
+Route::prefix('/')->middleware('login')->group(function () {
+    //home page
+    Route::get('/', 'PageController@home')->name('home.index');
+    Route::get('home', 'PageController@home')->name('home.index');
+    Route::get('sign-out', 'Auth\LoginController@logout')->name('logout');
 
-//timesheet & task
-Route::prefix('timesheets')->group(function () {
-    //timesheet
-    Route::get('/', 'TimesheetController@index')->name('timesheets.index');
-    Route::get('/create', 'TimesheetController@create')->name('timesheets.create');
-    Route::post('/create', 'TimesheetController@store')->name('timesheets.store');
-    Route::get('/{id}/edit', 'TimesheetController@edit')->name('timesheets.edit');
-    Route::post('/{id}/edit', 'TimesheetController@update')->name('timesheets.update');
-    Route::delete('/{id}/delete', 'TimesheetController@destroy')->name('timesheets.destroy');
+    //user
+    Route::prefix('user-profiles')->group(function () {
+        Route::get('/', 'UserController@index')->name('user.index');
+        Route::get('/edit', 'UserController@edit')->name('user.edit');
+        Route::post('/edit', 'UserController@update')->name('user.update');
+    });
 
-    // task
-    Route::get('/{ts_id}/tasks/create', 'TaskController@create')->name('tasks.create');
-    Route::post('/{ts_id}/tasks/create', 'TaskController@store')->name('tasks.store');
-    Route::get('/{ts_id}/tasks/{id}/edit', 'TaskController@edit')->name('tasks.edit');
-    Route::post('/{ts_id}/tasks/{id}/edit', 'TaskController@update')->name('tasks.update');
-    Route::delete('/{ts_id}/tasks/{id}/delete', 'TaskController@destroy')->name('tasks.destroy');
+    //timesheet & task
+    Route::prefix('timesheets')->group(function () {
+        //timesheet
+        Route::get('/', 'TimesheetController@index')->name('timesheets.index');
+        Route::get('/create', 'TimesheetController@create')->name('timesheets.create');
+        Route::post('/create', 'TimesheetController@store')->name('timesheets.store');
+        Route::get('/{id}/edit', 'TimesheetController@edit')->name('timesheets.edit');
+        Route::post('/{id}/edit', 'TimesheetController@update')->name('timesheets.update');
+        Route::delete('/{id}/delete', 'TimesheetController@destroy')->name('timesheets.destroy');
+
+        // task
+        Route::get('/{ts_id}/tasks/create', 'TaskController@create')->name('tasks.create');
+        Route::post('/{ts_id}/tasks/create', 'TaskController@store')->name('tasks.store');
+        Route::get('/{ts_id}/tasks/{id}/edit', 'TaskController@edit')->name('tasks.edit');
+        Route::post('/{ts_id}/tasks/{id}/edit', 'TaskController@update')->name('tasks.update');
+        Route::delete('/{ts_id}/tasks/{id}/delete', 'TaskController@destroy')->name('tasks.destroy');
+    });
 });
+ 
